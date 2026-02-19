841a1a20f9cafb063babb67264c1918377443b07
Paul <lazaro.roca.pol@gmail.com>
2026-02-19 10:24:11 +0100

    feature: step 4 completed

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from database import init_db, guardar_grup, carregar_grup
 app = Flask(__name__)
-@app.route("/")
+init_db()
+@app.route("/", methods=["GET", "POST"])
+
 def index():
-    return render_template("index.html")
+    noms_text=""
+    grup = 0
+    if request.method == "POST":
+        noms_text = request.form.get("noms")
+    if noms_text:
+        grup = [nom.strip() for nom in noms_text.split(",")]
+        guardar_grup(grup)
+        grup = carregar_grup()
+    
+    return render_template("index.html", grup=grup)
+    
 if __name__ == "__main__":
-    app.run(app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True)
