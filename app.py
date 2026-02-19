c21b63780201d14fbe810806b3358b3ccdbbbd5b
Paul <lazaro.roca.pol@gmail.com>
2026-02-19 10:00:29 +0100

    feature: step 2 completed

--- app.py
+++ app.py
@@ -4,4 +4,4 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(app.run(host="0.0.0.0", port=8000, debug=True)
